1be034a9ddc837ba570053161a26699f5c4b3222
Geordie Powers <geordie.powers@gmail.com>
2014-04-07 03:24:22 -0400

    - Removed everything from the old implementation

--- app/src/main/java/com/badlogic/androidgames/dontLetYourBallsDrop/Assets.java
+++
@@ -1,6 +0,0 @@
-package com.badlogic.androidgames.dontLetYourBallsDrop;
-
-public class Assets {
-    /* Resource Storage - For example; sounds, images, etc */
-    /* NOTE: Make variables in here static */
-}
